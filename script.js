c026c1eb059242b12ebd8073f2c07032e9c6d219
Vedant <vedantnp200@gmail.com>
2023-07-18 16:01:30 +0530

    Ford Explorer website using Flexbox

---
+++ script.js
@@ -0,0 +1,34 @@
+// Fixed Header
+window.onscroll = function() {
+    myFunction()
+};
+
+var header = document.getElementById("fixedHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+// responsive header
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
